5a45320d5d6c0d9db1cc90a46625f60d27cb9431
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-11 21:53:24 -0400

    refactor: Changing baseURL from Clash Royale Api to RoyaleAPI

--- utils/clashApi.js
+++ utils/clashApi.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 require('dotenv').config();
 
 const API = axios.create({
-  baseURL: 'https://api.clashroyale.com/v1',
+  //baseURL: 'https://api.clashroyale.com/v1',
+  baseURL: 'https://proxy.royaleapi.dev/v1',
   headers: {
     Authorization: `Bearer ${process.env.CLASH_API_KEY}`
   }
