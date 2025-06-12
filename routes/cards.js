a88f4c7a6bd6d10a55f82e9b99cb0b06258d1bae
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-12 00:00:38 -0400

    feat: Adding all cards an tournaments endpoints

---
+++ routes/cards.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const API = require('../utils/clashApi');
+
+// GET /cards
+router.get('/', async (req, res) => {
+    const { limit, after, before } = req.query; //Parametros no obligatorios
+
+    try {
+        const response = await API.get(`/clans`, {
+            params: { limit, after, before }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(error.response?.status || 500).json({
+            message: error.response?.data?.message || 'Error fetching cards'
+        });
+    }
+});
