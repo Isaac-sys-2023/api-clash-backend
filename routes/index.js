e27639483163107baf7d2609ce4d77f4986fbe05
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-11 23:51:23 -0400

    feat: Adding all players endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/clans', require('./clans'));
+router.use('/players', require('./players'));
 
 module.exports = router
