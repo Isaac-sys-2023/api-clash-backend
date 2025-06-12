8f953f0627339dcff9cf871287b453800c0268e2
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-12 00:07:07 -0400

    feat: Adding all routes on index.js

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,11 @@ const router = express.Router();
 
 router.use('/clans', require('./clans'));
 router.use('/players', require('./players'));
+router.use('/cards', require('./cards'));
+router.use('/tournaments', require('./tournaments'));
+router.use('/locations', require('./locations'));
+router.use('/challenges', require('./challenges'));
+router.use('/leaderboards', require('./leaderboards'));
+router.use('/globaltournaments', require('./globaltournaments'));
 
 module.exports = router
