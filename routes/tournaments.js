2c22c709c884e349097a9b11848b1c1a6c5d7307
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-13 00:14:47 -0400

    hotfix: rename tournamentTag variable to tournamentsTag

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -20,10 +20,10 @@ router.get('/', async (req, res) => {
 
 // GET /tournaments/:tournamentsTag
 router.get('/:tournamentsTag', async (req, res) => {
-    const { tournamentTag } = req.params; //Parametros no obligatorios
+    const { tournamentsTag } = req.params; //Parametros no obligatorios
 
     try {
-        const response = await API.get(`/tournaments/%23${tournamentTag}`);
+        const response = await API.get(`/tournaments/%23${tournamentsTag}`);
         res.json(response.data);
     } catch (error) {
         res.status(error.response?.status || 500).json({
