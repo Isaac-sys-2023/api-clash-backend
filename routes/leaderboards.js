69a4d8f09646907dedc15c041c768d9206524816
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-12 21:34:19 -0400

    hotfix: Oh GOD why? WHYYYYYYYYY?

--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -14,19 +14,19 @@ router.get('/', async (req, res) => {
     }
 });
 
-// GET /leaderboards/:leaderboardId
+// GET /leaderboard/:leaderboardId
 router.get('/', async (req, res) => {
     const { leaderboardId } = req.params; //Parametros obligatorios
     const { limit, after, before } = req.query; //Parametros no obligatorios
 
     try {
-        const response = await API.get(`/leaderboards/${leaderboardId}`, {
+        const response = await API.get(`/leaderboard/${leaderboardId}`, {
             params: { limit, after, before }
         });
         res.json(response.data);
     } catch (error) {
         res.status(error.response?.status || 500).json({
-            message: error.response?.data?.message || 'Error fetching leaderboards'
+            message: error.response?.data?.message || 'Error fetching leaderboard'
         });
     }
 });
