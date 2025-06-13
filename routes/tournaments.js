1f613a793ff1e6ba5063ca715febad06ec7d7780
Isaac Gutierrez Huarachi <gutierrezhuarachiisaac@gmail.com>
2025-06-13 00:08:31 -0400

    hotfix: fixing an tag error in tournaments

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -23,7 +23,7 @@ router.get('/:tournamentsTag', async (req, res) => {
     const { tournamentTag } = req.params; //Parametros no obligatorios
 
     try {
-        const response = await API.get(`/tournaments/${tournamentTag}`);
+        const response = await API.get(`/tournaments/%23${tournamentTag}`);
         res.json(response.data);
     } catch (error) {
         res.status(error.response?.status || 500).json({
